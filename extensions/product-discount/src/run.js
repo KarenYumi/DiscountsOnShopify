b109a7731a6cb4a1cb5c42820cb9a69d394887ba
Karen <karen.yumi20@gmail.com>
2025-04-09 17:12:32 -0300

    completing the function

--- extensions/product-discount/src/run.js
+++ extensions/product-discount/src/run.js
@@ -1,9 +1,12 @@
 // @ts-check
 import { DiscountApplicationStrategy } from "../generated/api";
 
+// Use JSDoc annotations for type safety
 /**
  * @typedef {import("../generated/api").RunInput} RunInput
  * @typedef {import("../generated/api").FunctionRunResult} FunctionRunResult
+ * @typedef {import("../generated/api").Target} Target
+ * @typedef {import("../generated/api").ProductVariant} ProductVariant
  */
 
 /**
@@ -13,15 +16,41 @@ const EMPTY_DISCOUNT = {
   discountApplicationStrategy: DiscountApplicationStrategy.First,
   discounts: [],
 };
-
 /**
  * @param {RunInput} input
  * @returns {FunctionRunResult}
  */
 export function run(input) {
-  const configuration = JSON.parse(
-    input?.discountNode?.metafield?.value ?? "{}"
-  );
+  const targets = input.cart.lines
+    // Only include cart lines with a quantity of two or more
+    .filter((line) => line.quantity >= 2)
+    .map((line) => {
+      return ({
+        // Use the cart line ID to create a discount target
+        cartLine: {
+          id: line.id,
+        },
+      });
+    });
+  if (!targets.length) {
+    // You can use STDERR for debug logs in your function
+    console.error("No cart lines qualify for volume discount.");
+    return EMPTY_DISCOUNT;
+  }
 
-  return EMPTY_DISCOUNT;
-};
+  return {
+    discounts: [
+      {
+        // Apply the discount to the collected targets
+        targets,
+        // Define a percentage-based discount
+        value: {
+          percentage: {
+            value: "10.0",
+          },
+        },
+      },
+    ],
+    discountApplicationStrategy: DiscountApplicationStrategy.First,
+  };
+}
